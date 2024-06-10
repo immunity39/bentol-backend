9e67da86e67e82b0b981ddcd158e0c78a71c9f90
yano.sou <22rd183@ms.dendai.ac.jp>
2024-06-10 17:51:32 +0900

    apiの修正, API-README.mdに修正方針を記載

--- README.md
+++ README.md
@@ -2,15 +2,16 @@
 backend側の設定等について
 
 ## API's
-- ログイン +json
+APIについての細かい設定は ./API-README.mdに記述があります。
+- ログイン
 POST /login
 - ショップ一覧
 GET /store
 - ショップの弁当一覧
 GET /store/{id}
 - ショップの弁当選択
-GET /store/{id}/bento/{id} or /store/{id}/{id}
-- 支払い + json
+GET /menue/{id}
+- 支払い
 POST /payment
 - 注文キャンセル
 DELETE /cancel/{id}
--- infrastructure/db.go
+++ infrastructure/db.go
@@ -3,20 +3,17 @@ package infrastructure
 import (
 	"fmt"
 	"log"
-	"os"
-	"path/filepath"
 
-	"gopkg.in/ini.v1"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 type ConfigList struct {
-	User     string
-	Password string
-	Host     string
-	Port     string
-	DBName   string
+	dbUser     string
+	dbPassword string
+	dbHost     string
+	dbPort     string
+	dbName     string
 }
 
 var Config ConfigList
@@ -24,37 +21,22 @@ var DB *gorm.DB
 
 func loadConfig() {
 	// Get the current directory
-	execDir, err := os.Getwd()
-	if err != nil {
-		log.Fatalf("Failed to get current directory: %v", err)
-	}
-
-	// Construct the config file path
-	configPath := filepath.Join(execDir, "infrastructure", "config.ini")
-
-	if _, err := os.Stat(configPath); os.IsNotExist(err) {
-		log.Fatalf("config.ini file does not exist at path: %s", configPath)
-	}
-
-	cfg, err := ini.Load(configPath)
-	if err != nil {
-		log.Fatalf("Failed to load config file: %v", err)
-	}
-
-	Config = ConfigList{
-		User:     cfg.Section("db").Key("user").String(),
-		Password: cfg.Section("db").Key("password").String(),
-		Host:     cfg.Section("db").Key("host").String(),
-		Port:     cfg.Section("db").Key("port").String(),
-		DBName:   cfg.Section("db").Key("dbname").String(),
-	}
+	//Config = ConfigList{
+	//    dbUser:     os.Getenv("dbUser")
+	//    dbPassword: os.Getenv("dbPassword")
+	//    dbHost:     os.Getenv("dbHost")
+	//    dbPort:     os.Getenv("dbPort")
+	//    dbName:     os.Getenv("dbName")
+	//}
 }
 
 func InitDB() {
-	loadConfig()
+	// loadConfig()
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		Config.User, Config.Password, Config.Host, Config.Port, Config.DBName)
+	//dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	//    Config.dbUser, Config.dbPassword, Config.dbHost, Config.dbPort, Config.dbName)
+
+	dsn := "%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local"
 
 	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
